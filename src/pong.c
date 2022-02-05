907e0bff8e8dee7498da8a356bba5a52bdb30c95
unnamed <brandylo@vo-i3.msk.21-school.ru>
2022-02-05 19:06:22 +0300

    fieldDraw func realese

--- src/pong.c
+++ src/pong.c
@@ -1,5 +1,28 @@
 //I WANT TO PLAY WITH YOU
 //        YOUR FRIEND, AI
+int fieldDraw(25/*widgth*/, 80/*lenght*/) {
+    for (int y = 0; y<=25; y++) {
+        for (int x = 1; x<=80; x++) {
+                if (((x==1) || (x==80)) && (y!=0)){ //отрисовка горизонтальных черт
+                    printf("%c",'|');
+                    if (x==80)
+                        printf("%c", '\n');
+                continue;
+            }
+            if((y==0)||(y==25)){ //отрисовка горизонтальных черты
+                printf("%c",'_');
+            }
+            if(x==80) { //абзац в конце строки
+                printf("%c",'\n');
+                continue;
+            }
+            if((y!=0)&&(y!=25)) { //ПРОБЕЛЫ
+                printf("%c",' ');
+                
+            }
+        }
+    }
+}
 
 
-
+cd .
