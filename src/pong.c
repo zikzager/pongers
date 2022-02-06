03456a5fdfe6a673864560e88afd92f7b32408cb
unnamed <brandylo@vo-h1.msk.21-school.ru>
2022-02-06 14:24:23 +0300

    fieldDraw func arguments added

--- src/pong.c
+++ src/pong.c
@@ -1,6 +1,6 @@
 //I WANT TO PLAY WITH YOU
 //        YOUR FRIEND, AI
-int fieldDraw(25/*widgth*/, 80/*lenght*/) {
+int fieldDraw(25/*widgth*/, 80/*lenght*/, posBoardCenterPlayer1, posBoardCenterPlayer1, posBallX, poBallY) {
     for (int y = 0; y<=25; y++) {
         for (int x = 1; x<=80; x++) {
                 if (((x==1) || (x==80)) && (y!=0)){ //отрисовка горизонтальных черт
@@ -11,8 +11,9 @@ int fieldDraw(25/*widgth*/, 80/*lenght*/) {
             }
             if((y==0)||(y==25)){ //отрисовка горизонтальных черты
                 printf("%c",'_');
+                continue;
             }
-            if(x==80) { //абзац в конце строки
+            if(x==80) { //абзац в конце строки (лишнее ибо первое условие)
                 printf("%c",'\n');
                 continue;
             }
@@ -24,5 +25,38 @@ int fieldDraw(25/*widgth*/, 80/*lenght*/) {
     }
 }
 
+int shiftPosYplayer1(int pos) {
+    getc(stdin); //фитрация ввода после  enter
+    char endCheck='\n';
+    scanf("%c%c", input, endCheck);
+    if (input == '\n')
+        return pos;
+    if (endCheck=='\n'){
+        if (input == 'a')
+            return (pos-1);
+        if (input == 'z') {
+            return (pos+1);
+        }
+    } else {
+        return pos;
+    }
+}
 
-cd .
+int shiftPosYplayer2(int pos) {
+    getc (stdin); //фитрация ввода после  enter
+    char endCheck='\n';
+    scanf("%c%c", input, endCheck);
+    if (input == '\n')
+        return pos;
+    if (endCheck=='\n'){
+        if (input == 'k')
+            return (pos-1);
+        if (input == 'm') {
+            return (pos+1);
+        }
+    } else {
+        return pos;
+    }
+}
+
+
